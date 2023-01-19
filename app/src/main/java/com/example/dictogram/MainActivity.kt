2a82ede3eb996d528bf7834c4e832960be73ae99
Supratim <pikucode15@gmail.com>
2023-01-19 15:01:20 +0530

    UI Changes

--- app/src/main/java/com/example/dictogram/MainActivity.kt
+++ app/src/main/java/com/example/dictogram/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.dictogram
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.window.SplashScreen
+import androidx.appcompat.widget.SearchView
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.ViewModel
 import com.example.dictogram.ViewModels.MainViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var splashViewModel: MainViewModel
+    private lateinit var searchWordSv: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen: androidx.core.splashscreen.SplashScreen = installSplashScreen()
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initVariables()
+
 
         splashScreen.apply {
             setKeepOnScreenCondition {
@@ -26,5 +31,6 @@ class MainActivity : AppCompatActivity() {
 
     private fun initVariables() {
         splashViewModel = MainViewModel()
+        searchWordSv = findViewById(R.id.search_word_sv)
     }
 }
